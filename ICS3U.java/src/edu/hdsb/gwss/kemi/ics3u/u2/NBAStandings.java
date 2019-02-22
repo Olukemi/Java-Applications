8c9831c926f399578e9ecf56a58350344368bc38
Olukemi <47333291+Olukemi@users.noreply.github.com>
2019-02-22 14:15:51 -0500

    Created NBA standings using print

---
+++ ICS3U.java/src/edu/hdsb/gwss/kemi/ics3u/u2/NBAStandings.java
@@ -0,0 +1,38 @@
+/*
+ * Name: Kemi
+ * Date: Feb 22 2019
+ * Version: v0.01
+ * Description: NBA Standings 
+ */
+package edu.hdsb.gwss.kemi.ics3u.u2;
+
+/**
+ *
+ * @author 1ODUJINRIKEM
+ */
+public class NBAStandings {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // These are the NBA Standings(i think, not sure what it means:\)
+        // I basically used sout Tab for the entire thing and used the dashes
+        
+        System.out.println("Eastern Conference");
+        System.out.println("Team                    W   L   Pct     GB ");
+        System.out.println("-------------------------------------------");
+        System.out.println("1   Bucks               43  14  .754    -  ");
+        System.out.println("-------------------------------------------");
+        System.out.println("2   Raptors             43  16  .729    1.0");
+        System.out.println("-------------------------------------------");
+        System.out.println("3   Pacers              38  20  .655    5.5");
+        System.out.println("-------------------------------------------");
+        System.out.println("4   Celtics             37  21  .638    6.5");
+        System.out.println("-------------------------------------------");
+        System.out.println("5   76ers               37  21  .638    6.5");
+       
+        
+    }
+    
+}

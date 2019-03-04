936d5ab85247fb0e8e20aad97ee0d13ad1ed599d
Olukemi <47333291+Olukemi@users.noreply.github.com>
2019-03-04 14:31:28 -0500

    Hall Cost

---
+++ ICS3U.java/src/edu/hdsb/gwss/kemi/ics3u/u2/HallCost.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.hdsb.gwss.kemi.ics3u.u2;
+
+/**
+ *
+ * @author 1ODUJINRIKEM
+ */
+public class HallCost {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

a38d185fb8994a16a3d2376e82faab6df33cb541
Olukemi <47333291+Olukemi@users.noreply.github.com>
2019-03-05 17:30:10 -0500

    change calculator

--- ICS3U.java/src/edu/hdsb/gwss/kemi/ics3u/u2/Change.java
+++ ICS3U.java/src/edu/hdsb/gwss/kemi/ics3u/u2/Change.java
@@ -9,13 +9,28 @@ package edu.hdsb.gwss.kemi.ics3u.u2;
  *
  * @author 1ODUJINRIKEM
  */
+import java.util.Scanner;
 public class Change {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Enter");
+                Scanner input = new Scanner(System.in);
+        System.out.println("Enter the change in cents less than a dollar:");
+        int change = input.nextInt();
+        
+        double quarter = change / 25;
+        double dime = Math.floor((change - (quarter * 25)) / 10);
+        double nickel = Math.floor(((change - (quarter * 25)) - dime * 10) / 5);
+        double penny = Math.floor(((change - (quarter * 25)) - dime * 10 - nickel * 5) / 1);
+    
+        System.out.println("The minimum number of coins is:");
+        
+        System.out.println("    Quarters: " + quarter);
+        System.out.println("    Dimes: " + dime);
+        System.out.println("    Nickels: " + nickel);
+        System.out.println("    Pennies " + penny);
     }
     
 }

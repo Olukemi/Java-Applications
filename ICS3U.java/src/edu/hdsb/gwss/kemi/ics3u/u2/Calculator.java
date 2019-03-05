91569f50eb52fecac2b685b0afd03cab6d873ac0
Olukemi <47333291+Olukemi@users.noreply.github.com>
2019-03-05 14:38:30 -0500

    Create Calculator.java

---
+++ ICS3U.java/src/edu/hdsb/gwss/kemi/ics3u/u2/Calculator.java
@@ -0,0 +1,37 @@
+/*
+ * Name: Kemi
+ * Date: Mar 8th 2019
+ * Version: v0.01
+ * Description: The application will prompt the user for the required input, 
+                complete the calculation, and display the results.
+ *
+ */
+package edu.hdsb.gwss.kemi.ics3u.u2;
+
+/**
+ *
+ * @author 1ODUJINRIKEM
+ */
+import java.util.Scanner;
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("WELCOME TO THE FUTURE VALUES CALCULATOR!");
+        System.out.println("Once you have inputed your regular payment per month "
+                + "this calculator will calculate the total ");
+        
+     
+        
+        //Constants:
+        //Variable:
+        //Input:
+        //Splash Page:
+        //Prossecing:
+        //Output:
+    }
+    
+}

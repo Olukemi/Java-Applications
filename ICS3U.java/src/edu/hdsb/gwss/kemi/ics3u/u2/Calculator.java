50fc21eda9cc246455a0b1266eef0051e0fc0773
Olukemi <47333291+Olukemi@users.noreply.github.com>
2019-03-06 16:59:47 -0500

    Update Calculator.java

--- ICS3U.java/src/edu/hdsb/gwss/kemi/ics3u/u2/Calculator.java
+++ ICS3U.java/src/edu/hdsb/gwss/kemi/ics3u/u2/Calculator.java
@@ -22,19 +22,18 @@ public class Calculator {
         Scanner input = new Scanner(System.in);
         //Splash Page:
         System.out.println("WELCOME TO THE FUTURE VALUES CALCULATOR!");
-        System.out.println("Once you have inputed your regular payment per month "
+        System.out.println("Once you have entered the total amount of money you would like to have\n "
                 + "this calculator will calculate the\n"
-                + "total amount of money earned by the end of your chosen time. An alternative\n"
-                + "to this calculator is calculating the required regular payment for a given\n"
+                + "the regular payment per month needed to get that amount during your chosen lenght of time.\n"
                 + "total amount of money from the user!\nLET'S BEGIN!");
         System.out.println("-----------------------------------------------------------------------------");
         
         //Constants:
         System.out.println("Please enter the length of time for each of your regular deposit:");
-        System.out.println("Enter your length of time you want to allow your "
+        System.out.println("Enter the number of years you want to allow your "
                 + "regular payment:");
-        System.out.println("Now enter the total amount of money you want at the end of your set period(#years):");
-        System.out.println("Enter interest rate per annum");
+        System.out.println("Now enter the total amount of money you want at the end of your set period(# of years):");
+        System.out.println("Enter your desired interest rate per year:");
         
         //Variables
         double regulartime_N;
@@ -45,18 +44,18 @@ public class Calculator {
        
         //Input:
         regulartime_N = input.nextDouble();
-        fulltime_n = input.nextDouble();
+        fulltime_n = input.nextDouble() * 12;
         totalamount_A = input.nextDouble();
-        interestrate_r = input.nextDouble();
-        interestrate_i = input.nextDouble();
+        interestrate_r = input.nextDouble() / 100.0;
+    
         
         //Prossecing:
         interestrate_i = interestrate_r / regulartime_N; 
-        double regularpayment_R = (totalamount_A * interestrate_i) / Math.pow((1 + interestrate_i), fulltime_n);
+        double regularpayment_R = Math.round((totalamount_A * interestrate_i) / (Math.pow((1 + interestrate_i), fulltime_n)-1));
         //Output:
         
-        System.out.println("Your regualar time payment is "  + regulartime_N + " a year");
-        System.out.println("Your total amount period is " + fulltime_n + "years");
+        System.out.println("Your regualar payment is "  + regularpayment_R + " a month for " + fulltime_n + " months");
+        
     }
     
 }

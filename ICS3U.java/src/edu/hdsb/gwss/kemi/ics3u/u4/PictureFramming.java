3b5555ef66aae511033cb471bc9571dddcafaf5c
Olukemi <47333291+Olukemi@users.noreply.github.com>
2019-04-03 13:22:58 -0400

    for loop for picture framming

--- ICS3U.java/src/edu/hdsb/gwss/kemi/ics3u/u4/PictureFramming.java
+++ ICS3U.java/src/edu/hdsb/gwss/kemi/ics3u/u4/PictureFramming.java
@@ -6,6 +6,7 @@
 package edu.hdsb.gwss.kemi.ics3u.u4;
 
 import java.util.Scanner;
+
 /**
  *
  * @author 1ODUJINRIKEM
@@ -22,15 +23,68 @@ public class PictureFramming {
 
         //INPUT
         System.out.println("Enter a value for the length of your picture:");
-        m = input.nextInt();
+        m = 3;//input.nextInt();
         System.out.println("Enter a value for the width of your picture:");
-        n = input.nextInt();
+        n = 4;//input.nextInt();
         System.out.println("Enter a value for the width of your mat:");
-        p = input.nextInt();
+        p = 1; //input.nextInt();
         System.out.println("Enter a value for the width of your frame:");
-        q = input.nextInt();
-     
+        q = 2; //input.nextInt();
+
         //PROCESSING
+        //TOP
+        for (int r = 0; r < q; r++) {
+            for (int i = 0; i < (2 * q + 2 * p + n); i++) {
+                System.out.print("#");
+            }
+            System.out.println("");
+        }
+        
+        for (int r = 0; r < p; r++) {
+            // LEFT FRAME
+            for (int i = 0; i < (q); i++) {
+                System.out.print("#");
+            }
+            
+            // MATTE
+            for (int i = 0; i < ( 2 * p + n); i++) {
+                System.out.print("+");
+            }
+            
+            // RIGHT FRAME
+            for (int i = 0; i < (q); i++) {
+                System.out.print("#");
+            }
+            System.out.println("");
+        }
+        
+        
+        
+        for (int r = 0; r < p; r++) {
+            // LEFT FRAME
+            for (int i = 0; i < (q); i++) {
+                System.out.print("#");
+            }
+            
+            // MATTE
+            for (int i = 0; i < ( 2 * p + n); i++) {
+                System.out.print("+");
+            }
+            
+            // RIGHT FRAME
+            for (int i = 0; i < (q); i++) {
+                System.out.print("#");
+            }
+            System.out.println("");
+        }
+        
+        //BOTTOM
+        for (int r = 0; r < q; r++) {
+            for (int i = 0; i < (2 * q + 2 * p + n); i++) {
+                System.out.print("#");
+            }
+            System.out.println("");
+        }
     }
-    
+
 }

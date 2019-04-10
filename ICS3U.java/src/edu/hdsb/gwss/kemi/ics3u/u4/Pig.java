688560af938fe2ebebc28486a62acfae3511db06
Olukemi <47333291+Olukemi@users.noreply.github.com>
2019-04-10 07:48:54 -0400

    beginning pig game code

--- ICS3U.java/src/edu/hdsb/gwss/kemi/ics3u/u4/Pig.java
+++ ICS3U.java/src/edu/hdsb/gwss/kemi/ics3u/u4/Pig.java
@@ -7,6 +7,8 @@ The point of the game is to get to 100 but there rules the code follows
  */
 package edu.hdsb.gwss.kemi.ics3u.u4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author 1ODUJINRIKEM
@@ -17,12 +19,31 @@ public class Pig {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         //CONSTANTS
         //VARIABLES
+        int die;
+        boolean play = true;
+        String quitOrPlay;
         //SPLASH PAGE
         //INPUT: N/A
         //PROCESSING & OUTPUT
-        
+        for (int userScore = 0, compScore = 0, userRoll = 0, compRoll = 0; userScore <= 100 && compScore <= 100;) {
+            while (play) {
+                die = (int) (Math.random() * 6) + 1;
+                System.out.println(die);
+                if (die != 1) {
+                    userScore = userScore + die;
+                    userRoll++;
+                } else if (die == 1){
+                    userScore = userScore;
+                    play = false;
+                    System.out.println("Do you want to continue playing? Enter 'Y' or 'N'");
+                    quitOrPlay = input.next();
+                }
+            }
+        }
+
     }
-    
+
 }

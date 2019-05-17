349b649460c582c305cd41a47bdbac4883f5c76a
Olukemi <47333291+Olukemi@users.noreply.github.com>
2019-05-17 13:24:30 -0400

    assignment

--- ICS3U.java/src/edu/hdsb/gwss/kemi/ics3u/u6/ReduceFraction.java
+++ ICS3U.java/src/edu/hdsb/gwss/kemi/ics3u/u6/ReduceFraction.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Name: Kemi
+ * Date: May 17th 2019
+ * Version: v0.01
+ * Description: This program will reduce any fraction into its lowest terms.
  */
 package edu.hdsb.gwss.kemi.ics3u.u6;
 

4e57041d8f334f1e7304627f284fe7426b9cfec9
Olukemi <47333291+Olukemi@users.noreply.github.com>
2019-05-29 13:26:22 -0400

    bubble sort

--- ICS3U.java/src/edu/hdsb/gwss/kemi/ics3u/u7/BubbleSort.java
+++ ICS3U.java/src/edu/hdsb/gwss/kemi/ics3u/u7/BubbleSort.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Name:
+ * Date:
+ * Version:
+ * Decription:
  */
 package edu.hdsb.gwss.kemi.ics3u.u7;
 
@@ -16,30 +17,45 @@ public class BubbleSort {
      */
     public static void main(String[] args) {
         int numbers[] = {20, 77, 41, 834, 196, 2, 111, 508, 16, 27};
+        display(numbers);
+        bubbleSort(numbers);
+        bubbleSort(numbers);
+        display(numbers);
+    }
+
+    public static void bubbleSort(int numbers[]) {
+        int swaps = 0;
+        int comps = 0;
+        boolean swapMade = true;
+        // MULTIPLE PASSES
+        for (int pass = 0; pass < numbers.length && swapMade; pass++) {
+            swapMade = false;
+            // PASS
+            for (int i = 0; i < numbers.length - 1 - pass; i++) {
+                if (numbers[i] > numbers[i + 1]) {
+                    swap(numbers, i, i + 1);
+                    swapMade = true;
+                    swaps++;
+                }
+                comps++;
+            }      
+        }
+        System.out.println("COMPS: " + comps );
+        System.out.println("SWAPS: " + swaps );
+    }
+
+    public static void swap(int numbers[], int i, int j) {//i = 4, j = 2
+        int extraHand = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = extraHand;
+
+    }
+
+    public static void display(int[] numbers) {
         for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i]);
+            System.out.print(numbers[i] + " ");
         }
         System.out.println(" ");
-        bubbleSort(numbers);
-    }
-    public static void bubbleSort(int numbers[]){
-        for (int i = 0, j = i+1; i <= numbers.length; i++){
-            if (numbers[i] >= numbers[j]){
-                swap(numbers, i, j);
-            } else if (numbers[i] <= numbers[j]){
-                System.out.println(numbers[i]);
-                System.out.println(numbers[i + 1]);
-            }
-        }
-    }
-    
-    public static void swap(int numbers[], int i, int j){//i = 4, j = 2
-        int holder = i;
-        i = j;
-        j = holder;
-        System.out.println(numbers[i]);
-        System.out.println(numbers[j]);
-
     }
 
 }
--- ICS3U.java/src/edu/hdsb/gwss/kemi/ics3u/u7/SelectionSort.java
+++ ICS3U.java/src/edu/hdsb/gwss/kemi/ics3u/u7/SelectionSort.java
@@ -15,7 +15,46 @@ public class SelectionSort {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int numbers[] = {20, 77, 41, 834, 196, 2, 111, 508, 16, 27};
+        display(numbers);
+        selectionSort(numbers);
+        selectionSort(numbers);
+        display(numbers);
     }
-    
+    public static void selectionSort(int numbers[]) {
+        int swaps = 0;
+        int comps = 0;
+        boolean swapMade = true;
+        // MULTIPLE PASSES
+        for (int pass = 0; pass < numbers.length && swapMade; pass++) {
+            swapMade = false;
+            // PASS
+            for (int i = 1, max = numbers[0]; i < numbers.length - 1 - pass; i++) {
+                if (numbers[i] > max) {
+                    max = numbers[i];
+                }
+                swap(numbers, i, i + 1);
+                    swapMade = true;
+                    swaps++;
+                comps++;
+            }
+            
+        }
+        System.out.println("COMPS: " + comps );
+        System.out.println("SWAPS: " + swaps );
+    }
+
+    public static void swap(int numbers[], int i, int j) {//i = 4, j = 2
+        int extraHand = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = extraHand;
+
+    }
+
+    public static void display(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+        System.out.println(" ");
+    }
 }
